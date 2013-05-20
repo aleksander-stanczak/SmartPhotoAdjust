8547264bc3709457681192ffe20936d5574fcc0a
mpiejdak <mateusz.piejdak@gmail.com>
2013-05-20 19:34:55 +0200

    noise detection

---
+++ src/output/OutputImageProccesor.java
@@ -0,0 +1,38 @@
+import ij.*;
+import ij.process.*;
+import ij.gui.*;
+import java.awt.*;
+import ij.plugin.filter.*;
+import ij.plugin.FFT;
+import ij.plugin.Histogram;
+
+public class OutputImageProccesor implements PlugInFilter {
+	private ImagePlus imp;
+	private String argument;
+	private int w,h;
+
+	public int setup(String arg, ImagePlus imp) {
+		this.imp = imp;
+		this.argument = arg;
+		return DOES_ALL;
+	}
+
+	public void run(ImageProcessor ip) {
+		w = ip.getWidth();
+		h = ip.getHeight();
+		ip.invert();
+		imp.updateAndDraw();
+		IJ.wait(500);
+		ip.invert();
+		imp.updateAndDraw();
+		getFFTMatix(argument);
+	}
+
+	private void getFFTMatix(String arg) {
+		FFT picture_fft_matrix = new FFT();
+		picture_fft_matrix.run(arg);
+		Histogram picture_fft_histogram = new Histogram();
+		picture_fft_histogram.run(arg);
+	}
+	
+}
