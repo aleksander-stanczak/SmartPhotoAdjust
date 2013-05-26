0f312be659b2a5e41ac5b3f92589e81357193768
mpiejdak <mateusz.piejdak@gmail.com>
2013-05-26 14:26:11 +0200

    input generator for 5 input's

--- src/input/InputGenerator.java
+++ src/input/InputGenerator.java
@@ -5,12 +5,28 @@ public class InputGenerator {
 	static public boolean[] getInputVector(){
 		
 		// gather info about image like contrast, histogram etc.
-		boolean[] imageInfo = {false,false,false,false};
+		boolean[] imageInfo = {false,false,false,false,false};
 		
 		InputNoiseCalc inc = new InputNoiseCalc();
+		InputBlurredCalc ibc = new InputBlurredCalc();
+		InputHueSaturationCalc ihsc = new InputHueSaturationCalc();
+		InputColorBalanceCalc icbc = new InputColorBalanceCalc();
+		InputBrightnessContrastCalc ibcc = new InputBrightnessContrastCalc();
 		
 		//inc.run(ip);
-		//imageInfo[0] = inc.isResult();
+		imageInfo[0] = inc.isResult();
+		
+		//icbc.run(ip);
+		imageInfo[1] = icbc.isResult();
+		
+		//ibcc.run(ip);
+		imageInfo[2] = ibcc.isResult();
+		
+		//ihsc.run(ip);
+		imageInfo[3] = ihsc.isResult();
+		
+		//ibc.run(ip);
+		imageInfo[4] = ibc.isResult();
 		
 		return imageInfo;
 	}
